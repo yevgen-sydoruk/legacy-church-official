96a80fbe4a4884aa3f712fedcfe0fe5654d41638
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2024-08-28 20:59:11 -0400

    Start of Navigation component
    Adding Navigation with basic li

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Legacy Church Official",
-  description: "Legacy Church in Chicopee Official Web Page",
+    title: 'Legacy Church Official',
+    description: 'Legacy Church in Chicopee Official Web Page'
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <a className="">Logo</a>
+      <ul>
+        <li>About</li>
+        <li>Get Involved</li>
+        <li>Watch</li>
+        <li>Give</li>
+        <li>I`m New</li>
+        <li>=</li>
+      </ul>
+    </div>
+  );
+}
