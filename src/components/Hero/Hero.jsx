fbb7e9386d40fb285d98000690a0ec5dfd065d43
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2024-09-10 00:07:09 -0400

    Adding svg logo to website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+// import LogoImage from "../../../assets/main-full-Logo.svg";
 import LogoImage from "../../../assets/main-full-Logo.png";
 import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className="h-lvh flex flex-col bg-hero-image bg-cover bg-center bg-no-repeat bg-fixed justify-center items-center opacity-90">
-      <Image src={LogoImage} alt="Main Legacy Church Logo" className="w-9/12 mb-16" />
+      <Image src={LogoImage} alt="Main Legacy Church Logo" className="w-9/12 mb-16 z-50" />
       <div className="flex gap-4 text-white">
         <button className="rounded-md border border-[#3498db] py-2 px-4 text-center text-2xl transition-all shadow-sm hover:shadow-lg text-white hover:text-white hover:bg-[#3498db] hover:border-[#3498db] focus:text-white focus:bg-[#3278a7] focus:border-[#3278a7] active:border-[#1d4560] active:text-white active:bg-[#1d4560] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none uppercase">
           Watch us online
