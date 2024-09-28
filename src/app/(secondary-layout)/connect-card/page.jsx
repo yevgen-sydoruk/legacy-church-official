2ac2287633c6911dfacc5a68857921d19f6540e8
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2024-09-27 20:04:31 -0400

    Connect Card done

--- src/app/(secondary-layout)/connect-card/page.jsx
+++ src/app/(secondary-layout)/connect-card/page.jsx
@@ -1,20 +1,18 @@
 import Banner from "@/components/Banner/Banner";
-import { ContactForm } from "@/components/ContactForms/ContactForm";
 import React from "react";
 import bannerImage from "../../../../public/legacy_youth_logo.png";
-import Link from "next/link";
-import { BaptismSignUpForm } from "@/components/ContactForms/BaptismSignUpForm";
+import { ConnectCardForm } from "@/components/ContactForms/ConnectCardForm";
 
 export const metadata = {
   title: "Legacy Church - MA - Mission And Vision",
   description: "Page description"
 };
 
-export default function BaptismSignup() {
+export default function ConnectCard() {
   return (
     <section className="pt-[120px]">
-      <Banner heading={"Baptism Sign-Up Form"} text={""} bannerImage={bannerImage} />
-      <div className="mx-auto max-w-6xl px-10 py-16 flex flex-col items-center text-center gap-5">
+      <Banner heading={"Connect Card"} text={""} bannerImage={bannerImage} />
+      {/* <div className="mx-auto max-w-6xl px-10 py-16 flex flex-col items-center text-center gap-5">
         <a href="https://www.bible.com/bible/97/MAT.28.18" target="_blank">
           <h2 className="mb-5 lg:text-6xl text-4xl font-light">
             "The Bible says that all professing Christians should be baptized"
@@ -25,12 +23,14 @@ export default function BaptismSignup() {
           Whether you've been following Jesus for a long time and never got baptized or are a new
           Christian, we would be honored to baptize you!
         </p>
-      </div>
+      </div> */}
       <div id="contactForm">
-        <BaptismSignUpForm
-          title={"Baptism Sign-Up Form"}
-          text={""}
-          sentFromPage={"Baptism Sign-Up"}
+        <ConnectCardForm
+          title={"Connect Card"}
+          text={
+            "Welcome to our church! We're glad you're here. Our team would love to serve you and help you get connected."
+          }
+          sentFromPage={"Connect Card"}
         />
       </div>
     </section>
