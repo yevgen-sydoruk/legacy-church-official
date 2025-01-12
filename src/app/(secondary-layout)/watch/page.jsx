1f8747ff5bf1622656c9f8427439a5320c17cb97
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2025-01-12 18:12:36 -0500

    Updated Watch Service for page, and component

--- src/app/(secondary-layout)/watch/page.jsx
+++ src/app/(secondary-layout)/watch/page.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import legacyYouthLogo from "../../../../public/legacy_youth_logo.png";
 import Link from "next/link";
 
+import LatestVideosBlock from "@/components/LatestVideosBlock/LatestVideosBlock";
+
 import YoutubeIcon from "../../../../assets/icons/YoutubeIcon";
 
 export const metadata = {
@@ -24,22 +26,15 @@ export const metadata = {
 export default function Watch() {
   return (
     <section className="pt-[120px]">
+      {/* Banner Component */}
       <Banner heading={"Watch Us Online"} text={""} bannerImage={legacyYouthLogo} />
       <div className="mx-auto px-10 py-8">
         <h2 className="lg:text-6xl text-4xl font-[300] text-center lato">
-          Watch Our Latest Sermon
+          Watch Our Latest Sermons
         </h2>
       </div>
-      <div className="mx-auto px-10 py-8 items-center text-center gap-5">
-        <iframe
-          src="https://www.youtube.com/embed/gD2EhIoHbyk?si=7VGk_i22Lt-u0l40"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="w-full aspect-video"
-        ></iframe>
-      </div>
+      <LatestVideosBlock />
+      {/* YouTube Channel Link */}
       <div className="flex justify-center py-8 text-3xl font-bold">
         <Link
           href="https://www.youtube.com/channel/UC93pSS2r4KNRLDelYoXbtbA"
@@ -48,10 +43,10 @@ export default function Watch() {
         >
           <a
             target="_blank"
-            className="flex flex-col text-center gap-4 items-center text-[#3498db] hover:text-cyan-700"
+            className="flex text-center gap-4 items-end text-[#3498db] hover:text-cyan-700"
           >
             Watch us online on YouTube
-            <YoutubeIcon styles="fill-[#3498db] hover:fill-cyan-700" />
+            <YoutubeIcon styles="fill-current" />
           </a>
         </Link>
       </div>
--- src/components/LatestVideosBlock/LatestVideosBlock.jsx
+++ src/components/LatestVideosBlock/LatestVideosBlock.jsx
@@ -2,7 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
+// import legacyYouthLogo from "../../../../public/legacy_youth_logo.png";
+// import Link from "next/link";
+// import YoutubeIcon from "../../../../assets/icons/YoutubeIcon";
 
 export default function WatchService() {
   const [videos, setVideos] = useState([]);
@@ -17,13 +19,12 @@ export default function WatchService() {
             channelId: process.env.NEXT_PUBLIC_CHANNEL_ID, // Channel ID
             eventType: "completed", // Completed live events
             type: "video", // Only video results
-            maxResults: 4, // Get the last 4 videos
-            order: "date", // Sort by date
+            maxResults: 4, // Get last 4 live videos
+            order: "date",
             key: process.env.NEXT_PUBLIC_YOUTUBE_KEY // YouTube Data API key
           }
         });
-
-        setVideos(response.data.items); // Set the video data
+        setVideos(response.data.items);
       } catch (error) {
         console.error("Error fetching live videos:", error);
       } finally {
@@ -45,17 +46,13 @@ export default function WatchService() {
   };
 
   return (
-    <section className="py-20 px-10 inset-0 bg-black/20" id="watchService">
-      <div className="container mx-auto flex flex-col justify-center items-center gap-10 text-center">
-        <h2 className="sm:text-6xl text-4xl">Watch Sunday Service Live</h2>
-      </div>
-
-      {/* Display the latest 4 live videos */}
+    <div>
+      {/* Display YouTube Video */}
       <div className="mx-auto px-10 py-8 items-center text-center gap-5">
         {loading ? (
-          <p>Loading live videos...</p>
+          <p>Loading Latest Sermon videos...</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
+          <div className="grid  sm:grid-cols-1 lg:grid-cols-2 gap-10">
             {videos.map(video => (
               <div key={video.id.videoId} className="flex flex-col items-center">
                 <iframe
@@ -66,26 +63,15 @@ export default function WatchService() {
                   allowFullScreen
                   className="w-full aspect-video"
                 ></iframe>
-                <p className="mt-4 text-lg font-bold">{video.snippet.title}</p>
-                <p className="mt-2 text-sm text-gray-500">
-                  {formatDate(video.snippet.publishedAt)}
+
+                <p className="mt-4 text-lg font-bold">
+                  {video.snippet.title} - {formatDate(video.snippet.publishedAt)}
                 </p>
               </div>
             ))}
           </div>
         )}
       </div>
-
-      <div className="flex flex-col justify-center items-center gap-5 py-8 text-xl font-bold">
-        <p className="max-w-3xl text-center">Can't make our service? Want to watch from home?</p>
-        <p className="max-w-3xl text-center">Catch our latest live streams here!</p>
-        <Link
-          className="rounded-md border text-[#3498db] bg-white hover:border-white py-2 px-4 text-center text-2xl transition-all shadow-lg hover:shadow-xl hover:text-white hover:bg-[#3498db] border-[#3498db] focus:text-white focus:bg-[#3278a7] focus:border-[#3278a7] active:border-[#1d4560] active:text-white active:bg-[#1d4560] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none uppercase"
-          href="watch"
-        >
-          Browse Sermons
-        </Link>
-      </div>
-    </section>
+    </div>
   );
 }
